363ffd5cb0bad8f4c3ffc0ed2c80c024601fa812
vijaya-madana <vijayamadana409@gmail.com>
2023-06-12 15:39:44 +0530

    changes in a file Task1.java

--- Task1.java
+++ Task1.java
@@ -1,7 +1,6 @@
 //1.1 Write a program to display any message
 
 //package com.basicjava;
-
 public class Task1 {
     public static void main(String[] args) {
         System.out.println(" Hi this is vijaya. \n I have completed my btech in bvcits with the stream of ECE. \n I have 3.1 years of experience in Coartha techno solutions pvt ltd as a software engineer");
