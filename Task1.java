3cf28a7318f805cba319105d2f38d0be482da85f
vijaya-madana <vijayamadana409@gmail.com>
2023-06-12 14:45:02 +0530

    added a file Task1.java

---
+++ Task1.java
@@ -0,0 +1,7 @@
+//package com.basicjava;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(" Hi this is vijaya. \n I have completed my btech in bvcits with the stream of ECE. \n I have 3.1 years of expwerience in Coartha techno solutions pvt ltd as a software engineer");
+    }
+}
